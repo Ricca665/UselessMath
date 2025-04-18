b3b04fb1b741df6928379bc52082e05e1a692dde
Ricca665 <riccardosarti2010@gmail.com>
2025-04-18 16:38:24 +0200

    added basic pypi package layout

--- tests/test_UselessMath.py
+++ tests/test_UselessMath.py
@@ -1,7 +1,10 @@
 import UselessMath
+import sys
 
 def test():
-    SqrtRange.Calculate(testfirstnumber, testsecondnumber, logFile, clearLog, True)
-    SqrtRange.CalculateWOLogging(testfirstnumber, testsecondnumber, True)
-    SqrtRange.CalculateWithThreads(testfirstnumber, testsecondnumber, logFile, clearLog, True, 2)
-    SqrtRange.CalculateWOLoggingWithThreads(testfirstnumber, testsecondnumber, True, 2)
+    if UselessMath.Add(2, 2) != 4:
+        sys.exit(1)
+    else:
+        print("Add function test passed!")
+
+test()
